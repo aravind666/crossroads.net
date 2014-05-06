5165809315edf1d77e32b6bc9109efec82b2cfbf
James Smith <james@gaslight.co>
2014-05-06 10:51:01 -0400

    get grunt-jekyll server running continuously

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,44 +8,37 @@ module.exports = function(grunt) {
 
    // Project configuration
   grunt.initConfig({
-      sass: {
-          dist: {
-              files: [
-                  {
-                      src: ['clientside/sass/main.scss'],
-                      dest: 'clientside/stylesheets/main.css'
-                  }
-              ]
-          }
-      },
-      cssmin: {
-          minify: {
-              expand: true,
-              cwd: 'clientside/stylesheets/',
-              src: ['*.css', '!*.min.css'],
-              dest: 'clientside/stylesheets/min/',
-              ext: '.min.css'
-          }
-      },
-      jekyll: {
-        server: {
+    sass: {
+        dist: {
+            files: [
+                {
+                    src: ['clientside/sass/main.scss'],
+                    dest: 'clientside/stylesheets/main.css'
+                }
+            ]
+        }
+    },
+    cssmin: {
+        minify: {
+            expand: true,
+            cwd: 'clientside/stylesheets/',
+            src: ['*.css', '!*.min.css'],
+            dest: 'clientside/stylesheets/min/',
+            ext: '.min.css'
+        }
+    },
+    jekyll: {
+      server: {
+        options: {
           dest: '_site',
           serve: true,
           server_port: 8000,
           auto: true
-			},
-      dev: {
-        dest: '_site'
-      }
-		},
-		watch: { // for development run 'grunt watch'
-			jekyll: {
-				files: ['templates/*.html'],
-				tasks: ['jekyll:dev']
-			}
-    }
+        }
+      },
+    },
   });
 
-  grunt.registerTask('default', ['sass','cssmin', 'jekyll:server']);
+  grunt.registerTask('default', ['sass','cssmin', 'jekyll']);
 
 }
