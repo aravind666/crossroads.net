52bb7262dc85473348d40e693331de7cc4ae4e2b
Michael Guterl <michael@diminishing.org>
2014-08-20 15:38:43 -0400

    Fix variable declaration

--- config/tasks/protractor.js
+++ config/tasks/protractor.js
@@ -3,11 +3,12 @@ var spawn = require('child_process').spawn,
 
 module.exports = function(gulp, $) {
   return gulp.task('protractor', ["webdriver_update", "jekyll_ci", "scripts"], function() {
-    var server;
-    server = spawn('node', ['server/server.js']);
+    var server = spawn('node', ['server/server.js']);
+
     server.stdout.on('data', function(data) {
       console.log('[ci server] ', data.toString());
     });
+
     gulp.src("spec/integration/**/*_spec.js").pipe(protractor({
       configFile: "config/protractor.conf.js",
            args: ['--baseUrl', 'http://127.0.0.1:8000']
