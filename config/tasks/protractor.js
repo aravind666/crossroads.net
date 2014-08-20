4a8f9b52fb1b43e03cc99c59819abe3b5a2449f8
Michael Guterl <michael@diminishing.org>
2014-08-20 15:26:07 -0400

    Add scripts as a dependent task to protractor

--- config/tasks/protractor.js
+++ config/tasks/protractor.js
@@ -2,7 +2,7 @@ var spawn = require('child_process').spawn,
     protractor = require("gulp-protractor").protractor;
 
 module.exports = function(gulp, $) {
-  return gulp.task('protractor', ["webdriver_update", "jekyll_ci"], function() {
+  return gulp.task('protractor', ["webdriver_update", "jekyll_ci", "scripts"], function() {
     var server;
     server = spawn('node', ['server/server.js']);
     server.stdout.on('data', function(data) {
